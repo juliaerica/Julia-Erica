63880df59f347226bf4cc65a035ab07b0c70a771
juliaerica <20221ireinfint0007@ifba.edu.br>
2025-05-24 09:59:02 -0300

    Continuando

---
+++ Calculadora.java
@@ -0,0 +1,36 @@
+package juliaericalista;
+
+public class Calculadora {
+    
+    public int somar (int a, int b){
+        
+        return a + b;
+    }
+    
+    public double somar (double a, double b){
+        
+        return a + b;
+    }
+    
+    public int somar (int [] valores){
+        int soma = 0;
+        for (int valor : valores) {
+            soma += valor;
+        }
+        return soma;
+    }
+    
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        int resultado1 = calc.somar(5, 7);
+        System.out.println("Soma de dois inteiros: " + resultado1);
+
+        double resultado2 = calc.somar(5.5, 7.3);
+        System.out.println("Soma de dois doubles: " + resultado2);
+
+        int[] numeros = {1, 2, 3, 4, 5};
+        int resultado3 = calc.somar(numeros);
+        System.out.println("Soma de um array de inteiros: " + resultado3);
+    }
+}
---
+++ Contato.java
@@ -0,0 +1,59 @@
+package juliaericalista;
+import java.util.HashSet;
+import java.util.Objects;
+
+public class Contato {
+    
+    String nome;
+    String email;
+    double telefone;
+
+    public Contato(String nome, String email, double telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+    
+     // Sobrescrevendo o toString()
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+    
+    // Sobrescrevendo o equals() para comparar pelo email
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Contato)) return false;
+        Contato outro = (Contato) obj;
+        return Objects.equals(email, outro.email);
+    }
+
+    // Sobrescrevendo o hashCode() também baseado no email
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    public static void main(String[] args) {
+        HashSet<Contato> contatos = new HashSet<>();
+
+        Contato c1 = new Contato("Ana", "ana@email.com", 1111-1111);
+        Contato c2 = new Contato("Bruno", "bruno@email.com", 2222-2222);
+        Contato c3 = new Contato("Carlos", "ana@email.com", 3333-3333); // Mesmo email que Ana
+
+        contatos.add(c1);
+        contatos.add(c2);
+        contatos.add(c3); // Este não será adicionado, pois tem o mesmo email que Ana
+
+        for (Contato c : contatos) {
+            System.out.println(c);
+        }
+    }
+    
+    
+}
