0962465e7509a89513d3a7d456e97e4b63f2341b
juliaerica <20221ireinfint0007@ifba.edu.br>
2025-05-23 21:10:57 -0300

    Primeiro commit

---
+++ Retangulo.java
@@ -0,0 +1,24 @@
+package juliaericalista;
+
+public class Retangulo {
+    
+    double largura;
+    double altura;
+    
+    Retangulo ( double altura, double largura){
+        
+        this.altura = altura;
+        this.largura = largura;
+    }
+    
+    double calcularArea (){
+        
+        return largura * altura;
+      
+    }
+    
+    double calcularPerimetro (){
+        
+        return 2 * largura + 2 * altura;
+    }
+}
